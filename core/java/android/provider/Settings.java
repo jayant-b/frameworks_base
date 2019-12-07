dad001527d932b89025f10842f0f22527dc4f457
SKULSHADY <anushekprasal@gmail.com>
2019-12-07 15:02:45 +0200

    base: Add refresh rate setting [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6755,6 +6755,12 @@ public final class Settings {
          */
         public static final String QS_DATAUSAGE = "qs_datausage";
 
+	/**
+	 * Refresh Rate Setting for Supported Devices
+         * @hide
+         */
+        public static final String REFRESH_RATE_SETTING = "refresh_rate_setting";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
